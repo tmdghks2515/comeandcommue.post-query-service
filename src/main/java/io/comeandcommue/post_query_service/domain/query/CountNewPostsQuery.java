ff0defbe4a0ebac5ee36aa815a8576a8550e191a
ish <tmdghks0615@gmail.com>
2025-09-22 15:46:53 +0900

    feat: 게시글 조회 파라미터 수정

--- src/main/java/io/comeandcommue/post_query_service/domain/query/CountNewPostsQuery.java
+++ src/main/java/io/comeandcommue/post_query_service/domain/query/CountNewPostsQuery.java
@@ -1,10 +1,10 @@
 package io.comeandcommue.post_query_service.domain.query;
 
-import java.time.LocalDateTime;
+import java.time.Instant;
 import java.util.List;
 
 public record CountNewPostsQuery(
         List<String> communityTypes,
-        LocalDateTime lastCreatedAt
+        Instant createdAtTo
 ) {
 }
--- src/main/java/io/comeandcommue/post_query_service/domain/query/RecentPostsQuery.java
+++ src/main/java/io/comeandcommue/post_query_service/domain/query/RecentPostsQuery.java
@@ -4,14 +4,15 @@ package io.comeandcommue.post_query_service.domain.query;
 import lombok.Getter;
 import lombok.Setter;
 
-import java.time.LocalDateTime;
+import java.time.Instant;
 import java.util.List;
 
 @Setter
 @Getter
 public class RecentPostsQuery {
     private List<String> communityTypes;
-    private LocalDateTime lastCreatedAt;
+    private Instant createdAtFrom;
+    private Instant createdAtTo;
     private int pageSize;
     private String userId;
 }
